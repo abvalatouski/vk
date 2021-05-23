1d2514e85fa879e700b5b460124bb6523c2a122d
Aliaksei Valatouski <abvalatouski@gmail.com>
2021-05-23 13:24:17 +0300

    Split code into several modules.

---
+++ examples/bot/app/ApiMethods.hs
@@ -0,0 +1,17 @@
+module ApiMethods
+  where
+
+import           Data.Text               (Text)
+import           Web.VK.Api.Mtl          ((@=))
+import qualified Web.VK.Api.Mtl          as VK
+import qualified Web.VK.Api.LongPoll.Mtl as VK
+
+-- See "https://vk.com/dev/messages.send".
+sendMessage :: VK.MonadApi m => Text -> VK.Id -> m ()
+sendMessage text userId = do
+    randomId <- VK.randomId
+    VK.callMethod_ "messages.send"
+        [ "peer_id"   @= userId
+        , "random_id" @= randomId
+        , "message"   @= text
+        ]
--- examples/bot/app/Main.hs
+++ examples/bot/app/Main.hs
@@ -1,69 +1,19 @@
 import           Control.Monad
 
-import           Control.Monad.Catch
-import           Control.Monad.State
 import           Data.FileEmbed          (embedStringFile)
-import           Data.HashMap.Strict     (HashMap, (!))
-import qualified Data.HashMap.Strict     as HashMap
-import           Data.Text               (Text)
-import           Lens.Micro
-import           Lens.Micro.TH
 import           Web.VK.Api.Mtl          ((@=))
 import qualified Web.VK.Api.Mtl          as VK
 import qualified Web.VK.Api.LongPoll.Mtl as VK
 
-data BotStatus = Active | Sleeping
-  deriving stock Show
-
-newtype BotState = BotState
-    { _botStatus :: BotStatus
-    }
-  deriving stock Show
-
-makeLenses ''BotState
-
-data Conversation = Conversation
-    { _conversationId       :: !VK.Id
-    , _conversationBotState :: !BotState
-    }
-  deriving stock Show
-
-makeLenses ''Conversation
-
-type Conversations = HashMap VK.Id Conversation
-
-conversation :: VK.Id -> Lens' Conversations Conversation
-conversation id = lens (! id) (flip $ HashMap.insert id)
-
-newtype BotM a = BotM
-    { runBotM :: StateT Conversations VK.LongPollM a
-    }
-  deriving newtype
-    ( Functor
-    , Applicative
-    , Monad
-    , MonadIO
-    , MonadState Conversations
-    , MonadThrow
-    , MonadCatch
-    )
-
-instance VK.MonadApi BotM where
-    askApiConn = BotM $ lift VK.askApiConn
-
-instance VK.MonadLongPoll BotM where
-    askLongPollServer = BotM $ lift VK.askLongPollServer
+import           ApiMethods
+import           Types
 
 main :: IO ()
-main = runBot bot
-
-runBot :: BotM a -> IO a
-runBot action = do
+main = do
     let apiToken   = $(embedStringFile "private/api-token")
         apiVersion = $(embedStringFile "private/api-version")
         groupId    = $(embedStringFile "private/group-id")
-    conn <- VK.mkApiConnDefault apiToken apiVersion
-    VK.runLongPollApi (evalStateT (runBotM action) []) groupId conn
+    runBot bot apiToken apiVersion groupId
 
 bot :: BotM ()
 bot = do
@@ -80,13 +30,3 @@ bot = do
                     pure ()
         _ ->
             pure ()
-
--- See "https://vk.com/dev/messages.send".
-sendMessage :: Text -> VK.Id -> BotM ()
-sendMessage text userId = do
-    randomId <- VK.randomId
-    VK.callMethod_ "messages.send"
-        [ "peer_id"   @= userId
-        , "random_id" @= randomId
-        , "message"   @= text
-        ]
