59d3bc01f59ba5baf69d0865ab1be40529f2f856
Aliaksei Valatouski <abvalatouski@gmail.com>
2021-05-23 14:44:27 +0300

    Remove unused import.

--- examples/bot/app/ApiMethods.hs
+++ examples/bot/app/ApiMethods.hs
@@ -1,10 +1,9 @@
 module ApiMethods
   where
 
-import           Data.Text               (Text)
-import           Web.VK.Api.Mtl          ((@=))
-import qualified Web.VK.Api.Mtl          as VK
-import qualified Web.VK.Api.LongPoll.Mtl as VK
+import           Data.Text      (Text)
+import           Web.VK.Api.Mtl ((@=))
+import qualified Web.VK.Api.Mtl as VK
 
 -- See "https://vk.com/dev/messages.send".
 sendMessage :: VK.MonadApi m => Text -> VK.Id -> m ()
